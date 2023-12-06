007cb393f453aae393107b99a66d490ae6d50a91
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2023-12-06 22:10:42 +0700

    express

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+const router = require("./routes");
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
